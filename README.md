7b3b4c78689f0d0f4d78f92e071b2248ea47f982
Tomoyuki Sakurai <y@trombik.org>
2021-10-22 18:44:08 +0700

    imp: support Ubuntu
    with a huge hack

--- README.md
+++ README.md
@@ -1,6 +1,47 @@
-# ansible-role-opensearch_dashboards
+# `trombik.opensearch_dashboards`
 
-A brief description of the role goes here.
+Manages `opensearch-dashboards`.
+
+Note that the latest `opensearch-dashboards` (1.1.0 as of this writing) has
+`nodejs` version 10.x and several outdated, vulnerable `nodejs` modules.
+See [Issue 835](https://github.com/opensearch-project/OpenSearch-Dashboards/issues/835)
+for more details. Generally, you should not use `openseach-dashboards` in
+production until the upstream releases a newer, fixed version. If this is your
+concern, use `elasticsearch` and `kibana` instead.
+
+## For FreeBSD users
+
+The package in the official FreeBSD ports tree (1.1.0 as of this writing) is
+broken. See
+[bug 259330](https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=259330) for
+more details. The role requires my own port, which can be found at
+[trombik/freebsd-ports-opensearch](https://github.com/trombik/freebsd-ports-opensearch).
+The port depends on old, deprecated `node10`. `node10` has been removed from
+the ports tree. As a result, my repository includes changes to `node` ports,
+reviving `node10`. In short, do not use it unless you know what you are doing.
+
+## For Debian users
+
+The role installs `opensearch-dashboards` from the official tar archive. This
+is a huge hack until when Amazon or distributions release packages.
+
+Changes from the default includes:
+
+* the configuration directory is `/etc/opensearch-dashboards`
+* log file is under `/var/log/opensearch-dashboards`
+* `path.data` is `/var/lib/opensearch-dashboards`
+* the application is installed under `/var/www/opensearch-dashboards`
+* the user to run the application is `www-data`
+
+The changes will be updated when an official package is available.
+
+The role downloads the official tar archive under
+`opensearch_dashboards_src_dir`. The default is `/var/dist` The directory is
+not just a cache directory. In addition to the tar file, it has a PGP key, a
+signature file , and files to control `ansible` tasks.
+
+The role installs a `systemd` unit file for `opensearch-dashboards`. The
+author is not an expert of `systemd` in any way.
 
 # Requirements
 
@@ -19,6 +60,26 @@ None
 # Example Playbook
 
 ```yaml
+---
+- hosts: localhost
+  roles:
+    - ansible-role-opensearch_dashboards
+  vars:
+    opensearch_dashboards_config:
+      server.host: "{{ opensearch_dashboards_bind_address }}"
+      server.port: "{{ opensearch_dashboards_bind_port }}"
+      path.data: "{{ opensearch_dashboards_data_dir }}"
+      opensearch.hosts: ["http://localhost:9200"]
+      opensearch.ssl.verificationMode: none
+      opensearch.username: "kibanaserver"
+      opensearch.password: "kibanaserver"
+      opensearch.requestHeadersWhitelist:
+        - authorization,securitytenant
+      opensearch_security.multitenancy.enabled: true
+      opensearch_security.multitenancy.tenants.preferred: ["Private", "Global"]
+      opensearch_security.readonly_mode.roles: ["kibana_read_only"]
+      # Use this setting if you are running kibana without https
+      opensearch_security.cookie.secure: false
 ```
 
 # License
--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -11,9 +11,116 @@ config_dir = case os[:family]
                "/etc/opensearch-dashboards"
              end
 config = "#{config_dir}/opensearch_dashboards.yml"
+install_method = case os[:family]
+                 when "ubuntu"
+                   "src"
+                 else
+                   "default"
+                 end
+data_dir = case os[:family]
+           when "freebsd"
+             "/var/db/openseach-dashboards"
+           else
+             "/var/lib/opensearch-dashboards"
+           end
+default_user = "root"
+default_group = case os[:family]
+                when /bsd/
+                  "wheel"
+                else
+                  "root"
+                end
+user = case os[:family]
+       when "freebsd"
+         "www"
+       else
+         "www-data"
+       end
 
-describe package(package) do
-  it { should be_installed }
+group = case os[:family]
+        when "freebsd"
+          "www"
+        else
+          "www-data"
+        end
+log_dir = case os[:family]
+          when "freebsd"
+            "/var/log"
+          else
+            "/var/log/opensearch-dashboards"
+          end
+src_dir = "/var/dist"
+src_dist_name = "opensearch-dashboards-1.1.0"
+src_root_dir = "/var/www/opensearch-dashboards"
+bin_dir = case os[:family]
+             when "freebsd"
+               "/usr/local/www/opensearch-dashboards/bin"
+             when "ubuntu"
+               "#{src_root_dir}/bin"
+             end
+
+case install_method
+when "src"
+  describe file src_dir do
+    it { should exist }
+    it { should be_directory }
+    it { should be_mode 755 }
+    it { should be_owned_by default_user }
+    it { should be_grouped_into default_group }
+  end
+
+  describe file src_root_dir do
+    it { should exist }
+    it { should be_directory }
+    it { should be_mode 755 }
+    it { should be_owned_by default_user }
+    it { should be_grouped_into default_group }
+  end
+
+  describe file "#{bin_dir}/opensearch-dashboards" do
+    it { should exist }
+    it { should be_file }
+    it { should be_mode 775 }
+    it { should be_owned_by default_user }
+    it { should be_grouped_into default_group }
+  end
+
+  %w[ sig_verified key_verified key_imported src_extracted ].each do |f|
+    describe file "#{src_dir}/.#{f}-#{src_dist_name}" do
+      it { should exist }
+      it { should be_file }
+      it { should be_mode 644 }
+      it { should be_owned_by default_user }
+      it { should be_grouped_into default_group }
+    end
+  end
+
+  describe file "/etc/systemd/system/opensearch-dashboards.service" do
+      it { should exist }
+      it { should be_symlink }
+  end
+
+  describe file "/lib/systemd/system/opensearch-dashboards.service" do
+      it { should exist }
+      it { should be_file }
+      it { should be_mode 644 }
+      it { should be_owned_by default_user }
+      it { should be_grouped_into default_group }
+      its(:content) { should match(/Managed by ansible/) }
+  end
+
+  describe file "/home/vagrant/.gnupg" do
+    it { should_not exist }
+  end
+
+  describe file "/root/.gnupg" do
+    it { should exist }
+    it { should be_directory }
+  end
+else
+  describe package(package) do
+    it { should be_installed }
+  end
 end
 
 describe file(config) do
@@ -21,9 +128,39 @@ describe file(config) do
   its(:content) { should match Regexp.escape("Managed by ansible") }
 end
 
+describe file data_dir do
+  it { should exist }
+  it { should be_directory }
+  it { should be_owned_by user }
+  it { should be_grouped_into group }
+  it { should be_mode 755 }
+end
+
+describe file log_dir do
+  case os[:family]
+  when "freebsd"
+    # XXX /var/log
+    it { should exist }
+    it { should be_directory }
+  else
+    it { should exist }
+    it { should be_directory }
+    it { should be_owned_by user }
+    it { should be_grouped_into group }
+    it { should be_mode 755 }
+  end
+end
+
 describe service(service) do
   it { should be_running }
   it { should be_enabled }
+end
+
+describe command "#{bin_dir}/opensearch-dashboards-plugin list" do
+  let(:sudo_options) { "-u #{user} " }
+  its(:exit_status) { should eq 0 }
+  its(:stderr) { should eq "" }
+  its(:stdout) { should match(/securityDashboards/)}
 end
 
 ports.each do |p|
